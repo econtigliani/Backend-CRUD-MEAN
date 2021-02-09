a357a23d08c68ee89bb703fcf07553f3f67032b1
Emanuel Contigliani <emanuelconti@hotmail.com>
2021-02-09 18:14:21 -0300

    configuración de rutas

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ const conectarDB = require('./config/db')
 
 const app = express()
 conectarDB();
-app.get('/', (req,res)=>{
-    res.send('Hola mundo!')
-})
 
+app.use('/api/productos', require('./routes/producto'))
 app.listen(4000,()=>{
     console.log('el servidor esta corriendo correctamente')
 })
