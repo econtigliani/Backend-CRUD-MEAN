8b48aa4f62c2d1a75512fb4175180a5a0e760a2f
Emanuel Contigliani <emanuelconti@hotmail.com>
2021-02-09 19:11:57 -0300

    Creación de producto

--- controllers/productoController.js
+++ controllers/productoController.js
@@ -1,3 +1,17 @@
-exports.crearProducto = (req, res) => {
-    console.log(req.body)
+const Producto = require( "../models/Producto");
+
+
+exports.crearProducto= async (req, res) => {
+    try {
+        //Creamos nuestro producto
+        
+        let producto = new Producto(req.body)
+        await producto.save()
+        res.send(producto)
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error')
+        
+    }
 }
