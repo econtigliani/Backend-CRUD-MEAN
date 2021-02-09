9b66bfb66933f4e88defbcd3a5630351b5021462
Emanuel Contigliani <emanuelconti@hotmail.com>
2021-02-09 16:39:56 -0300

    configuracion de servidor

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('desde index.js')
+const exrpress = require('express')
+
+const app = exrpress()
+
+app.get('/', (req,res)=>{
+    res.send('Hola mundo!')
+})
+
+app.listen(4000,()=>{
+    console.log('el servidor esta corriendo correctamente')
+})
