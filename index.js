6f97873143f604f4c9f0434ac52e891dcac95de1
Emanuel Contigliani <emanuelconti@hotmail.com>
2021-02-09 22:33:58 -0300

    Api terminada

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const conectarDB = require('./config/db')
+const cors = require('cors')
 
 
 const app = express()
 conectarDB();
+app.use(cors())
 app.use(express.json())
+
 app.use('/api/productos', require('./routes/producto'))
 app.listen(4000,()=>{
     console.log('el servidor esta corriendo correctamente')
